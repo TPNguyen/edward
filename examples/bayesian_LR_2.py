4ca7a9f50e7ff411f065539a5a60b75ba2a5ab6a
G.Zhang <GongZ@dhcp-128-189-230-63.ubcsecure.wireless.ubc.ca>
2018-04-25 17:24:12 -0700

    Add file for multi dimension

--- examples/bayesian_LR_2.py
+++ examples/bayesian_LR_2.py
@@ -5,21 +5,32 @@ from edward.models import Bernoulli, Empirical, Normal
 import matplotlib.pyplot as plt
 import math
 
+flags = tf.app.flags
 
-N = 50000  # number of data points
-D = 1  # number of features
-T = 1000  # number of empirical samples 
+flags.DEFINE_integer("N", default=50000, help="Number of data points.")
+flags.DEFINE_integer("D", default=1, help="Number of features.")
+flags.DEFINE_integer("T", default=2000, help="Number of posterior samples.")
+flags.DEFINE_string("logdir", default="log", help="The directory to save log")
 
-def build_toy_dataset(N, noise_std=0.1):
-  x = np.random.normal(loc=0.0, scale = 1.0, size=N) 
+# add a new flag to pass in sampler
+flags.DEFINE_string("sampler", default="HMC", help="The sampler to use, HMC(Hamiltonian Monte Carlo), SGHMC(Stochastic Hamiltonian Monte Carlo), SGLD(Stochastic gradient Langevin dynamics)")
+
+
+FLAGS = flags.FLAGS
+
+
+
+def build_toy_dataset(noise_std=0.1):
+  x = np.random.normal(loc=0.0, scale = 1.0, size=FLAGS.N*FLAGS.D) 
+  x = x.reshape((FLAGS.N, FLAGS.D)) 
+  b = np.linspace(1.0, 10.0, num=FLAGS.D)
 
   # b = 1.0  
   # b = np.array((np.float64)(b))
   # print(x.shape)
   # print(b.shape)
-  y = x + np.random.normal(0, noise_std, size=N) # b = 1 
+  y = ed.dot(x, b) + np.random.normal(0, noise_std, size=FLAGS.N) # b = 1 
   y = Bernoulli(logits=y) 
-  x = x.reshape((N, D))
   return x, y
 
 
@@ -29,19 +40,28 @@ def build_toy_dataset(N, noise_std=0.1):
 # x_data = np.zeros([N, D])
 # y_data = np.zeros([N])
 
-x_data, y_data = build_toy_dataset(N, 0.1)
+x_data, y_data = build_toy_dataset(0.1)
 
 # MODEL
 x = tf.Variable(x_data, trainable=False, dtype = tf.float32)
 # print(type(x))
-beta = Normal(loc=tf.zeros(D), scale=tf.ones(D))
+beta = Normal(loc=tf.zeros(FLAGS.D), scale=tf.ones(FLAGS.D))
 # print(type(beta))
 y = Bernoulli(logits=ed.dot(x, beta))
 
 # INFERENCE
-qbeta = Empirical(params=tf.Variable(tf.zeros([T, D])))
-inference = ed.HMC({beta: qbeta}, data={y: y_data})
-inference.run(step_size=10/N, n_steps=10)
+qbeta = Empirical(params=tf.Variable(tf.zeros([FLAGS.T, FLAGS.D])))
+
+if (FLAGS.sampler == "SGHMC"):
+  inference = ed.SGHMC({beta: qbeta}, data={y: y_data})
+elif (FLAGS.sampler == "SGLD"):
+  inference = ed.SGLD({beta: qbeta}, data={y: y_data})
+elif (FLAGS.sampler == "HMC"):
+  inference = ed.HMC({beta: qbeta}, data={y: y_data})
+
+# inference.run(step_size=10/N, n_steps=10)
+inference.run(step_size=10/FLAGS.N, n_steps=10) 
+
 
 sess = ed.get_session() 
 trace_beta = sess.run(qbeta.params)
@@ -70,8 +90,9 @@ def ess(traces, g = None):
   return (iid_var/sigma)*(traces.shape[0])
 
 
-qw_ess = ess(trace_beta)  
-print(qw_ess)
+beta_ess = ess(trace_beta)  
+print("Effect Sampling Size is: ")
+print(beta_ess)
 
 # traceb = sess.run(qb.params)
 # print(type(trace_w))
